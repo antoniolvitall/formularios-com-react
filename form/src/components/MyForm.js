c8d38f66cc46dc4f634fb486fc66ff567ac47b9d
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-13 17:27:42 -0300

    gerenciamento de dados de input

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,13 +1,23 @@
-import './MyForm.css'
+import './MyForm.css';
+import {useState} from 'react';
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const[name, setName] = useState()
+  const[email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+
   return (
     <div>
         {/* 1- criacao de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" />
+                <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
             </div>
             {/* 2 - label envolvendo imput */}
             <label>
