cd0a0f7d2f359f812ce626ca1997602c63474d41
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-13 18:00:38 -0300

    controlled inputs

--- form/src/App.js
+++ form/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm/>
+      <MyForm user={{name: "Antonio", email: "antoniolvitall@gmail.com"}}/>
     </div>
   );
 }
--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,10 +1,13 @@
 import './MyForm.css';
 import {useState} from 'react';
 
-const MyForm = () => {
+const MyForm = ({user}) => {
+  // 6 - controlled inputs
+
+
   // 3 - gerenciamento de dados
-  const[name, setName] = useState()
-  const[email, setEmail] = useState()
+  const[name, setName] = useState(user ? user.name: "")
+  const[email, setEmail] = useState(user ? user.email: "")
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -27,13 +30,13 @@ const MyForm = () => {
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
+                <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} value={name} />
             </div>
             {/* 2 - label envolvendo imput */}
             <label>
               <span>E-mail:</span>
               {/* 4 - simplicacao de manipulacao de state */}
-              <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} />
+              <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value)} value={email} />
             </label>
             <input type="submit" value="Enviar" />
         </form>    
